2a39233585a8f5f89be09f169b267cc19e42505e
Trần Anh Đức <ducanhtran28112x@gmail.com>
2024-11-06 19:15:19 +0700

    update code UI bán hàng

--- src/components/Admin/Sale/CheckoutComponent.tsx
+++ src/components/Admin/Sale/CheckoutComponent.tsx
@@ -1,5 +1,14 @@
-import React, {memo} from "react";
-import {Button, Drawer, Space} from "antd";
+import React, {memo, useState} from "react";
+import {AutoComplete, AutoCompleteProps, Button, Divider, Drawer, Flex, Space, Typography} from "antd";
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
+import {PlusOutlined, SearchOutlined} from "@ant-design/icons";
+
+const {Title, Text} = Typography;
+
+
+const mockVal = (str: string, repeat = 1) => ({
+    value: str.repeat(repeat),
+});
 
 interface IProps {
     title?: string;
@@ -8,27 +17,58 @@ interface IProps {
 }
 
 const CheckoutComponent: React.FC<IProps> = (props) => {
-    const {title, open, onClose} = props;
+    const {open, onClose} = props;
+    const [options, setOptions] = useState<AutoCompleteProps['options']>([]);
+
+    const getPanelValue = (searchText: string) =>
+        !searchText ? [] : [mockVal(searchText), mockVal(searchText, 2), mockVal(searchText, 3)];
 
     return (
         <Drawer
-            title={title}
+            title={
+                <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                    <div style={{display: "flex", width: "60%"}}>
+                        <AutoComplete allowClear suffixIcon={<SearchOutlined/>} style={{width: "100%"}}
+                                      options={options}
+                                      onSearch={(text) => setOptions(getPanelValue(text))}
+                                      placeholder="Tìm khách hàng"
+                        />
+                        <Button icon={<PlusOutlined/>} type="text" shape="circle"/>
+                    </div>
+                    <div>
+                        <Button onClick={onClose} type="text" icon={<CloseIcon/>}/>
+                    </div>
+                </Flex>
+            }
             placement="right"
             size="large"
             onClose={onClose}
             open={open}
-            extra={
-                <Space>
-                    <Button onClick={onClose}>Cancel</Button>
-                    <Button type="primary" onClick={onClose}>
-                        OK
-                    </Button>
-                </Space>
-            }
+            closable={false}
         >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                <Title level={3}>Trần Anh Đức 0123456789</Title>
+                <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                    <Text style={{fontSize: 16}}>Tổng tiền hàng</Text>
+                    <Text style={{fontSize: 16}} strong>1,000,000</Text>
+                </Flex>
+                <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                    <Text style={{fontSize: 16}}>Giảm giá</Text>
+                    <Text style={{fontSize: 16}} strong>1,000,000</Text>
+                </Flex>
+            </Flex>
+            <Divider/>
+            <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                    <Text style={{fontSize: 16}} strong>Khách cần trả</Text>
+                    <Text style={{fontSize: 16}} strong>1,000,000</Text>
+                </Flex>
+                <Flex justify="space-between" align="center" style={{width: "100%"}} wrap>
+                    <Text style={{fontSize: 16}} strong>Khách thanh toán</Text>
+                    <Text style={{fontSize: 16}} strong>1,000,000</Text>
+                </Flex>
+            </Flex>
+            <Divider/>
         </Drawer>
     );
 }
