41dfe86b0b0802ba286d895a58041d97f643943a
quangvdph31622 <quangvdph31622@fpt.edu.vn>
2024-11-08 23:36:49 +0700

    update user

--- src/components/User/Cart/TotalAmount.tsx
+++ src/components/User/Cart/TotalAmount.tsx
@@ -11,25 +11,10 @@ const TotalAmount = () => {
                         <div className="coupon">
                             <Form action="#" target="_blank">
                                 <Input name="Coupon" placeholder="Nhập mã giảm giá của bạn" style={{width: 300}}/>
-                                <Button className="btn">Apply</Button>
+                                <Button className="btn">Áp dụng</Button>
                             </Form>
                         </div>
-                        <div className="row mt-3">
-                            <div className="checkbox">
-                                <div>
-                                    <label className="p-0">
-                                        <Checkbox><p>Miễn phí vận chuyển (20.000 VND)</p></Checkbox>
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="checkbox">
-                                <div>
-                                    <label className="p-0">
-                                        <Checkbox><p>Giảm giá 10% cho toàn bộ sản phẩm (Tối đa: 15.000 VND)</p></Checkbox>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
                 <div className="col-lg-4 col-md-7 col-12">
@@ -42,13 +27,13 @@ const TotalAmount = () => {
                         </ul>
                         <div className="button5">
                             <Link
-                                href="#"
+                                href="/checkout"
                                 className="btn btn-dark d-flex align-items-center justify-content-center"
                             >
                                 Thanh toán
                             </Link>
                             <Link
-                                href="#"
+                                href="/category"
                                 className="btn btn-dark d-flex align-items-center justify-content-center"
                             >
                                 Tiếp tục mua hàng
--- src/components/User/ShopSingle/ProductDescription.tsx
+++ src/components/User/ShopSingle/ProductDescription.tsx
@@ -2,7 +2,7 @@
 
 import React, {useState} from 'react';
 import Link from "next/link";
-import {Input, Rate} from 'antd';
+import {Button, Input, Rate} from 'antd';
 import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
 import {FaRegHeart} from "react-icons/fa";
 import {LiaCompressArrowsAltSolid} from "react-icons/lia";
@@ -54,26 +54,97 @@ const ProductDescription = () => {
                 <p className="price"><span className="discount">$70.00</span><s>$80.00</s></p>
                 <p className="description">Sản phẩm được thiết kế tiện lợi và phù hợp cho mọi gia đình...</p>
             </div>
-            <div className="color">
-                <h4>Tùy chọn có sẵn <span>Màu sắc</span></h4>
-                <ul>
-                    <li><Link href="#" className="one"><i className="ti-check"></i></Link></li>
-                    <li><Link href="#" className="two"><i className="ti-check"></i></Link></li>
-                    <li><Link href="#" className="three"><i className="ti-check"></i></Link></li>
-                    <li><Link href="#" className="four"><i className="ti-check"></i></Link></li>
+            <div
+                className="color"
+                style={{
+                    marginBottom: "20px",
+                    padding: "10px",
+                }}
+            >
+                <h4
+                    style={{
+                        fontSize: "18px",
+                        fontWeight: "bold",
+                        marginBottom: "10px",
+                    }}
+                >
+                    Tùy chọn có sẵn <span>Màu sắc</span>
+                </h4>
+                <ul
+                    style={{
+                        display: "flex",
+                        listStyle: "none",
+                        padding: "0",
+                        margin: "0",
+                    }}
+                >
+                    <li
+                        style={{
+                            marginRight: "10px",
+                        }}
+                    >
+                        <Link href="#" className="one">
+                            <i className="ti-check"></i>
+                        </Link>
+                    </li>
+                    <li
+                        style={{
+                            marginRight: "10px",
+                        }}
+                    >
+                        <Link href="#" className="two">
+                            <i className="ti-check"></i>
+                        </Link>
+                    </li>
+                    <li
+                        style={{
+                            marginRight: "10px",
+                        }}
+                    >
+                        <Link href="#" className="three">
+                            <i className="ti-check"></i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="four">
+                            <i className="ti-check"></i>
+                        </Link>
+                    </li>
                 </ul>
             </div>
-            <div className="size">
-                <h4>Kích cỡ</h4>
-                <ul>
+
+            <div
+                className="size"
+                style={{
+                    marginBottom: "20px",
+                    padding: "10px",
+                }}
+            >
+                <h4
+                    style={{
+                        fontSize: "18px",
+                        fontWeight: "bold",
+                        marginBottom: "10px",
+                    }}
+                >
+                    Kích cỡ
+                </h4>
+                <ul
+                    style={{
+                        display: "flex",
+                        listStyle: "none",
+                        padding: "0",
+                        margin: "0",
+                    }}
+                >
                     {['S', 'M', 'L', 'XL', 'XXL'].map((size) => (
-                        <li key={size}>
+                        <li key={size} style={{ marginRight: "10px" }}>
                             <Link
                                 href="#"
                                 className={
-                                    selectedSize === size ?
-                                        'selected-size link-no-decoration' :
-                                        'link-no-decoration'
+                                    selectedSize === size
+                                        ? 'selected-size link-no-decoration'
+                                        : 'link-no-decoration'
                                 }
                                 onClick={(e) => {
                                     e.preventDefault();
@@ -86,6 +157,7 @@ const ProductDescription = () => {
                     ))}
                 </ul>
             </div>
+
             <div className="product-buy">
                 <div className="quantity">
                     <h6>Số lượng :</h6>
@@ -121,11 +193,36 @@ const ProductDescription = () => {
                             </button>
                         </div>
                     </div>
+                    {/*<div className="input-group">*/}
+                    {/*    <Button*/}
+                    {/*        icon={<AiOutlineMinus style={{color: '#fff'}} />}*/}
+                    {/*        onClick={handleDecrement}*/}
+                    {/*        disabled={quantity <= 1}*/}
+                    {/*        style={{background: '#F7941D', height: "50px"}}*/}
+                    {/*    />*/}
+                    {/*    <Input*/}
+                    {/*        type="text"*/}
+                    {/*        name="quant[1]"*/}
+                    {/*        className="input-number"*/}
+                    {/*        value={quantity}*/}
+                    {/*        onInput={handleInputChange}*/}
+                    {/*        onBlur={handleInputBlur}*/}
+                    {/*        style={{width: "60px", border: '1px solid #333', textAlign: "center"}}*/}
+                    {/*        readOnly*/}
+                    {/*    />*/}
+                    {/*    <Button*/}
+                    {/*        icon={<AiOutlinePlus style={{color: '#fff'}} />}*/}
+                    {/*        onClick={handleIncrement}*/}
+                    {/*        disabled={quantity >= 1000}*/}
+                    {/*        style={{background: '#F7941D'}}*/}
+                    {/*    />*/}
+                    {/*</div>*/}
+
                 </div>
                 <div className="add-to-cart">
                     <Link href={"/cart"} className="btn">Thêm vào giỏ hàng</Link>
                 </div>
-                <p className="cat">Category :<Link href="#" className="link-no-decoration">Clothing</Link></p>
+                <p className="cat">Danh mục :<Link href="#" className="link-no-decoration">Áo</Link></p>
                 <p className="availability">Tình trạng : 180 Sản phẩm còn hàng</p>
             </div>
         </div>
