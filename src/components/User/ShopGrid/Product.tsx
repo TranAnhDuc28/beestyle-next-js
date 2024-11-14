12267001700bd4212c398f5a01f60dcb534cae10
loc131024 <locnhph38787@fpt.edu.vn>
2024-11-14 22:10:57 +0700

    update

--- src/components/User/ShopGrid/Product.tsx
+++ src/components/User/ShopGrid/Product.tsx
@@ -134,39 +134,43 @@ const Product = () => {
     },
   ];
 
-    return (
-        <section className="product-area shop-sidebar shop section pb-1" style={{backgroundColor: '#fff'}}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3 col-md-4 col-12">
-                        <div className="shop-sidebar">
-                            <Category/>
-                            <ShopByPrice/>
-                            <RecentPosts/>
-                            <Manufacturers/>
-                        </div>
-                    </div>
-                    <div className="col-lg-9 col-md-8 col-12">
-                        <div className="row">
-                            <div className="col-12">
-                                <FilterProduct/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            {products.map((product, index) => (
-                                <div className="col-lg-4 col-md-6 col-12" key={index}>
-                                    <ProductList product={product}/>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className="row mt-5">
-                </div>
+  return (
+    <section
+      className="product-area shop-sidebar shop section pb-1"
+      style={{ backgroundColor: "#fff" }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-4 col-12">
+            <div className="shop-sidebar">
+              <Category />
+              <ShopByPrice />
+              <RecentPosts />
+              <Manufacturers />
             </div>
-        </section>
-
-    );
+          </div>
+          <div className="col-lg-9 col-md-8 col-12">
+            <div className="row">
+              <div className="col-12">
+                <FilterProduct />
+              </div>
+            </div>
+            <div className="row">
+              {products.map((product, index) => (
+                <div className="col-lg-3 col-md-6 col-12" key={index}>
+                  <ProductList product={product} />
+                </div>
+              ))}
+            </div>
+            <div className="mt-5 flex justify-center">
+              <Pagination defaultCurrent={1} total={50} />
+            </div>
+          </div>
+        </div>
+        <div className="row mt-5"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Product;
